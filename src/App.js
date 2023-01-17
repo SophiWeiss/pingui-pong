dd018ac97f356e75982784c6f5a86587acb507f1
Sophi Weiss <Owlety34@gmail.com>
2023-01-17 20:26:10 +0100

    some vital starting changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 
 function App() {
-
   return <div className="App">
     <h1 className="h1">PINGUI PONG</h1>
-    <p className="p">This is my<strong> Pingui Pong</strong>, in this menu u can change colors as <strong>your
-      ass</strong> wants to!</p>
-    <div className="div_button" id="main-menu">
-      <button className="button" >PLAY</button>
-      <button className="button" id="preferences-button">CHANGE COLORS</button>
-    </div>
+    <p className="p">This is my<strong> Pingui Pong</strong>, in this menu u can change colors as <strong>your ass</strong> wants to!</p>
+    <button className="button">PLAY</button>
+    <button className="button">CHANGE COLORS</button>
   </div>
-
 }
 
 export default App;
