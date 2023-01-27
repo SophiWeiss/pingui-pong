c27747f2253a8b4f45b4ed58a34f7bc58f3026d8
Papsan <papsanlysenko@gmail.com>
2023-01-28 00:07:56 +0200

    minor changes

--- src/Window.js
+++ src/Window.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import './Window.css'
 
+function BackButton(props) {
+  return (
+    <button className='back-button' onClick={props.onClick} style={getGradientStyle(['#993955', '#8b85c1'])}>
+      <img src="https://img.icons8.com/sf-black-filled/64/ffffff/left.png" alt='back-button'/>
+    </button>
+  )
+}
+
 function Window(props) {
   return (
     <div className='window'>
       {props.children}
+      {props.showBackButton && <BackButton onClick={props.onClick}/>}
     </div>
-  )
-}
-
-function BackButton(props) {
-  return (
-    <button className='back-button' onClick={props.onClick}>
-      {props.children}
-    </button>
   )
 }
 
 function getGradientStyle(colors) {
   const colorsCount = colors.length
-  const colorsWithPercentage = colors.map((color, index) => {
-    return `${color} ${Math.round(100 * index / (colorsCount - 1))}%`
-  })
+  const colorsWithPercentage = colors.map((color, index) =>
+    `${color} ${Math.round(100 * index / (colorsCount - 1))}%`
+  )
 
   return {backgroundImage: `linear-gradient(45deg, ${colorsWithPercentage.join(', ')})`}
 }
@@ -58,7 +59,7 @@ function ColorsControl({colorMap}) {
 
 export function StartWindow(props) {
   return (
-    <Window>
+    <Window showBackButton={false}>
       <h1>PINGUI PONG</h1>
       <p>
         This is my<strong> Pingui Pong</strong>, in this menu u can change colors as <strong>your ass</strong> wants to!
@@ -71,8 +72,8 @@ export function StartWindow(props) {
 
 export function ChangeColorsWindow(props) {
   return (
-    <Window>
-      <h1 className="h1">CHANGE COLORS</h1>
+    <Window showBackButton={true} onClick={props.onClick}>
+      <h1>CHANGE COLORS</h1>
       <ColorsControl colorMap={[
         {
           'MOUNTAINS': ['#993955', '#8b85c1', '#fae3e3'],
@@ -89,9 +90,6 @@ export function ChangeColorsWindow(props) {
           'SUMMER': ['#50514f', '#f25f5c', '#ffe066', '#70c1b3']
         }
       ]}/>
-      <BackButton onClick={props.onClick}>
-        <img src="https://img.icons8.com/ios-glyphs/30/ffffff/circled-left.png" alt='back-button'/>
-      </BackButton>
     </Window>
   )
 }
