09d38ffa9be22f28b49b0f7deca604629b1c31e1
Sophi Weiss <Owlety34@gmail.com>
2023-01-28 16:42:15 +0100

    game css and js files created

---
+++ src/components/game.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Canvas extends React.Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  updateCanvas () {
+
+  }
+
+  render () {
+    return (Canvas);
+  }
+
+}
